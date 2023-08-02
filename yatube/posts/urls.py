a077d7760e3fdda27e79bf549287b9057d63cdb5
Artur Volnov <fristail95@gmail.com>
2023-08-02 18:10:51 +0500

    changed html and add requirements

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_main_page
+from .views import get_main_page, get_group
 
 urlpatterns = [
     path('', get_main_page),
+    path('group/', get_group),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,3 +5,6 @@ def get_main_page(request):
     template = 'index.html'
     return render(request, template)
     
+def get_group(request):
+    template = 'group_list.html'
+    return render(request, template)
